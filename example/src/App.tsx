0be68545719d87ddc97ee5ba3e9424b15d07765e
sagar4596 <132884303+sagar4596@users.noreply.github.com>
2023-10-17 12:28:25 +0530

    intial commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-admanager-example';
+import CustomAdComponent from 'react-native-admanager-example';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <CustomAdComponent docereeId={'DOC-412-1'} />
+      <Text>Result:</Text>
+      <CustomAdComponent docereeId={'DOC-397-1'} />
     </View>
   );
 }
@@ -29,3 +25,27 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
 });
+
+// const response = {
+//   response: [
+//     {
+//       status: 1,
+//       adUnit: 'DOC-16-1',
+//       adText: 'Sample Ad',
+//       adImg:
+//         'https://www.wordstream.com/wp-content/uploads/2021/07/banner-ads-examples-aws.jpg',
+//       position: 1,
+//       adFormat: 1,
+//       adSize: '468 x 60',
+//       script:
+//         "<div><a href='https://doceree.com/' target='_blank'><img onload=\"(function registerImpression(){var img = document.createElement('img'); img.src='https://doceree.com/'; document.body.append(img);}).call(this)\" src='https://dev-servedbydoceree.doceree.com/generic/468x60_DocereeGeneralAd.jpg' /></a></div>",
+//       userId: 'DE.V1.12048346472e.1696486835530',
+//       bidRequestId: 'e46c953c-983c-4349-ac13-f9e5b93b7fa7-carvk',
+//       impressionId: '63c37a0a-e3e0-435c-8f97-8bc2f19da0d7-carvk',
+//       adRenderURL:
+//         'https://dai.doceree.com/drs/nEvent?bidPrice=0.0&bidRequestId=e46c953c-983c-4349-ac13-f9e5b93b7fa7-carvk&bidStrategy=1&userId=DE.V1.12048346472e.1696486835530&eType=1&eventClientTime={{EVENT_CLIENT_TIME}}',
+//       adViewedURL:
+//         'https://dai.doceree.com/drs/nEvent?bidPrice=0.0&bidRequestId=e46c953c-983c-4349-ac13-f9e5b93b7fa7-carvk&impressionId=63c37a0a-e3e0-435c-8f97-8bc2f19da0d7-carvk&userId=DE.V1.12048346472e.1696486835530&bidStrategy=1&eType=2&viewedPercentage={{VIEWED_PERCENTAGE}}&viewedTime={{VIEWED_TIME}}&eventClientTime={{EVENT_CLIENT_TIME}}',
+//     },
+//   ],
+// };
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,73 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { View, Image, Text, Linking } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const AD_URL = 'https://qa-ad-test.doceree.com/drs/quest';
+
+const CustomAdComponent = (props: any) => {
+  const [adData, setAdData] = useState<any>({});
+
+  useEffect(() => {
+    async function fetchAdData() {
+      const obj = {
+        adunit: props.docereeId,
+        hcpid: '1674332725209',
+        specialization: 'Surgery',
+        userid: 'ffffffff-d6b2-b41d-ffff-ffffef05ac4a',
+        zipcode: '65201',
+      };
+      const response = await fetch(AD_URL, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+      });
+      const adData = await response.json();
+      console.log(adData);
+      setAdData(adData.response[0]);
+    }
+    fetchAdData();
+  }, []);
+
+  if (!adData) {
+    return null;
+  }
+
+  if (typeof adData.adFormat === 'undefined') {
+    return (
+      <WebView
+        source={{
+          html: `<h1>This is my HTML content</h1>
+          <p>This is a paragraph of text.</p>`,
+        }}
+      />
+    );
+  }
+
+  return (
+    <TouchableOpacity onPress={() => Linking.openURL(adData?.adViewedURL)}>
+      <Image
+        source={{ uri: adData?.imagePath }}
+        style={{
+          height: parseInt(
+            typeof adData?.adSize !== 'undefined'
+              ? adData.adSize.split('x')[1]
+              : 0
+          ),
+          width: parseInt(
+            typeof adData?.adSize !== 'undefined'
+              ? adData.adSize.split('x')[0]
+              : 0
+          ),
+        }}
+      />
+      <Text>{adData?.data?.response[0]?.adText}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomAdComponent;
